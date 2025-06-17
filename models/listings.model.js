03ae66763153d124738274717c9ed7dc51255b5f
Akshat Jain <gomat2230@gmail.com>
2025-06-17 09:49:46 +0530

    update listings model

--- models/listings.model.js
+++ models/listings.model.js
@@ -18,6 +18,10 @@ const listingSchema = new mongoose.Schema({
     },
     location: [
         {
+            houseAddress:{
+                type: String,
+                required: true
+            },
             city: {
                 type: String,
                 required: true,
